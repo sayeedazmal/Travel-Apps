76ebec6f6be93130d179db1942ed636e104bc508
sayeedazmal <sayeedazmal01@gmail.com>
2018-12-09 10:16:26 +0600

    locations add activity

---
+++ app/src/main/java/com/example/mdazmal/travel/LocationFind.java
@@ -0,0 +1,14 @@
+package com.example.mdazmal.travel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocationFind extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_find);
+    }
+}
